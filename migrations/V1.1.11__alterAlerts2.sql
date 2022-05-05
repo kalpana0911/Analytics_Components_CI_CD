ed772c6793c04f57bd21a5c170b615d554981487
Anitha Singamsetti <Anitha.Singamsetti@elysiumanalytics.ai>
2022-05-05 20:35:03 +0530

    test10

---
+++ migrations/V1.1.11__alterAlerts2.sql
@@ -0,0 +1,3 @@
+use SCHEMA ci_cd_schema;
+alter TABLE  alert_test2 add column info variant; 
+alter TABLE  alert_test2 add column info2 variant; 
--- migrations/VW1.1.11__alterAlerts2.sql
+++
@@ -1,3 +0,0 @@
-use SCHEMA ci_cd_schema;
-alter TABLE  alert_test1 add column info variant; 
-alter TABLE  alert_test1 add column info2 variant; 
